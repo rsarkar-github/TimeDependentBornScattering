1c32ff4a6b2d84d460870986dd000361487786fa
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2023-05-02 00:55:04 -0700

    Updates to P02

--- Scripts/p02a_marmousi_cig_plot.py
+++ Scripts/p02a_marmousi_cig_plot.py
@@ -41,7 +41,7 @@ def marmousi_cig_plot(scale_fac, figdir, datadir, nx, nz, cig_aspect, thread_num
     locs = [0.3, 0.4, 0.5, 0.6, 0.7]
 
     for item in locs:
-        
+
         cig = dm_image[0:int(dm_image.shape[0] / 2), int(params["Nx"] * item), :].T
 
         # Apply WB mute (sample 22)
