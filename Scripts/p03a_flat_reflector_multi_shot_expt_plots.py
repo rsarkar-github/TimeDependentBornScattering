69b2f0a008ed731c5be5edc002bef1b0bd02302b
Rahul Sarkar <rahulsarkar.iitkgp@gmail.com>
2023-05-08 23:08:42 -0700

    Updates p03a* Script

--- Scripts/p03a_flat_reflector_multi_shot_expt_plots.py
+++ Scripts/p03a_flat_reflector_multi_shot_expt_plots.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
 
     # Plot stack, depth slices, and CIGs through inverted stack
     dm_scale = 0.1
-    cig_aspect = 8
+    cig_aspect = 6
     def plot_stack_slices_cigs():
 
         # Locations for CIGs
